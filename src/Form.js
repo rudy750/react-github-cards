dbb36e1db506d278c40f036a6805f467631da18d
Rudy Sarmiento <rudy750@gmail.com>
2021-11-23 22:59:07 -0500

    changed to a function

--- src/Form.js
+++ src/Form.js
@@ -1,33 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class Form extends Component {
-  state = { userName: "" };
-  handleSubmit = async (event) => {
+export default function Form(props) {
+  const [userName, setUserName] = useState("");
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const resp = await fetch(
-      `https://api.github.com/users/${this.state.userName}`
-    );
+    const resp = await fetch(`https://api.github.com/users/${userName}`);
     const user = await resp.json();
-    this.props.onSubmit(user);
-    this.setState({ userName: "" });
+    props.onSubmit(user);
+    setUserName("");
     console.log(user);
   };
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="GitHub username"
-            required
-            value={this.state.userName}
-            onChange={(event) =>
-              this.setState({ userName: event.target.value })
-            }
-          />
-          <button>Add card</button>
-        </form>
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="GitHub username"
+          required
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button>Add card</button>
+      </form>
+    </div>
+  );
 }
