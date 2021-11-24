ffedf1faba8d3e7d67f946b1ad69a0aa858ed78f
Rudy Sarmiento <rudy750@gmail.com>
2021-11-24 10:45:21 -0500

    added class declaration for reference

--- src/Form.js
+++ src/Form.js
@@ -5,9 +5,10 @@ export default function Form(props) {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const resp = await (
-      await fetch(`https://api.github.com/users/${userName}`)
-    ).json();
+    const resp = await fetch(`https://api.github.com/users/${userName}`).then(
+      (resp) => resp.json()
+    );
+
     // const user = await resp.json();
     props.onSubmit(resp);
     setUserName("");
@@ -29,3 +30,41 @@ export default function Form(props) {
     </div>
   );
 }
+
+/**
+ * 
+ * see the difference between class/functional compone
+ * 
+ * 
+ * export default class Form extends Component {
+  state = { userName: "" };
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const resp = await fetch(
+      `https://api.github.com/users/${this.state.userName}`
+    );
+    const user = await resp.json();
+    this.props.onSubmit(user);
+    this.setState({ userName: "" });
+    console.log(user);
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="GitHub username"
+            required
+            value={this.state.userName}
+            onChange={(event) =>
+              this.setState({ userName: event.target.value })
+            }
+          />
+          <button>Add card</button>
+        </form>
+      </div>
+    );
+  }
+}
+ */
